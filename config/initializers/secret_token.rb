26046bb94e4f5104305757acf9da1f2b31d529cb
dohkogt <kenp777@gmail.com>
2010-11-11 12:16:58 -0600

    subida de rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taller::Application.config.secret_token = 'fee8eecfcead83af85517f02cb7f2dcad4557c8a8c27165032c2d63f6b27783e08f0dc960a57c7770564fde550edec53ced2fc3fde10c6c57ddb437099534252'
